26749c93ad8d4231083015ea1714a098dd3aa59f
xrr2016 <xiaoranran1993@163.com>
2017-04-03 14:30:10 +0800

    :bug: fixed

--- config/passport.js
+++ config/passport.js
@@ -1,10 +1,12 @@
 const JwtStrategy = require('passport-jwt').Strategy
+const ExtractJwt = require('passport-jwt').ExtractJwt
 const User = require('../src/models/user')
 const config = require('../config/database')
 
 module.exports = (passport) => {
   let options = {}
   options.secretOrKey = config.secret
+  options.jwtFromRequest = ExtractJwt.fromAuthHeader()
   passport.use(new JwtStrategy(options, function (jwtPayload, done) {
     User.findOne({id: jwtPayload.id}, function (err, user) {
       if (err) {
--- src/models/user.js
+++ src/models/user.js
@@ -3,21 +3,24 @@ const bcrypt = require('bcrypt')
 
 const userSchema = new mongoose.Schema({
   username: { type: String, uniuqe: true, required: true },
-  password: { type: String, required: true }
+  password: { type: String, required: true },
+  created_at: {type: Date, default: Date.now},
+  update_at: {type: Date, default: Date.now}
 })
 
-userSchema.pre('save', (next) => {
+userSchema.pre('save', function (next) {
   const user = this
   if (this.isModified('password') || this.isNew) {
-    bcrypt.genSalt(10, (err, salt) => {
+    bcrypt.genSalt(10, function (err, salt) {
       if (err) {
         return next(err)
       }
-      bcrypt.hash(user.password, salt, (err, hash) => {
+      bcrypt.hash(user.password, salt, function (err, hash) {
         if (err) {
           return next(err)
         }
         user.password = hash
+        user.update_at = Date.now()
         next()
       })
     })
@@ -26,8 +29,8 @@ userSchema.pre('save', (next) => {
   }
 })
 
-userSchema.methods.comparePassword = (password, cb) => {
-  bcrypt.compare(password, this.password, (err, isMatch) => {
+userSchema.methods.comparePassword = function (password, cb) {
+  bcrypt.compare(password, this.password, function (err, isMatch) {
     if (err) {
       return cb(err)
     }
